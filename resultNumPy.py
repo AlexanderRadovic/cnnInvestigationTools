d17d26b341b15ada30863873250e9e96ffa133dd
AlexanderRadovic <a.radovic@gmail.com>
2018-03-22 16:49:50 -0500

    Spring cleaning and comments

--- resultNumPy.py
+++ resultNumPy.py
@@ -1,4 +1,5 @@
 #Create numpy file of network outputs and true labels, and a file of simple arrays of the input "images"
+#Used as an input to the tsne macros.
 
 import sys
 import caffe
@@ -11,6 +12,7 @@ import ROOT
 from collections import defaultdict
 
 if __name__ == "__main__":
+    #take network and data information from the command line
     parser = argparse.ArgumentParser()
     parser.add_argument('--proto', type=str, required=True)
     parser.add_argument('--model', type=str, required=True)
@@ -22,18 +24,17 @@ if __name__ == "__main__":
     matrix = defaultdict(int) # (real,pred) -> int
     labels_set = set()
 
+    #setup caffe network, set leveldb address
     net = caffe.Net(args.proto, args.model, caffe.TEST)
     caffe.set_mode_gpu()
     db = leveldb.LevelDB(args.leveldb)
-    #lmdb_env = lmdb.open(args.lmdb)
-    #lmdb_txn = lmdb_env.begin()
-    #lmdb_cursor = lmdb_txn.cursor()
 
     numpy_data=[]
     numpy_images=[]
 
     cutcount=0
     
+    #loop over events, saving network outputs and inputs in a numpy array
     for key, value in db.RangeIter():
         if count > 100000:
             break
@@ -45,6 +46,8 @@ if __name__ == "__main__":
         image = caffe.io.datum_to_array(datum)
         image = image.astype(np.uint8)
         count = count + 1
+        
+        #for events with enough activity to be interesting, store data for tsne plots
         if np.count_nonzero(np.asarray([image])) > 40:
             cutcount=cutcount+1
         
@@ -71,6 +74,8 @@ if __name__ == "__main__":
 
     print "\r"
     print 100.*cutcount/count
+    
+    #save arrays locally
     numpy_data_as_array=np.array(numpy_data)
     np.save('dataCos.npy',numpy_data_as_array)
     numpy_images_as_array=np.array(numpy_images)
